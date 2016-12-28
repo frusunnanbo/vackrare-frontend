e9e64a5f1a802f283c79a7c6b599f602f821c810
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2016-12-28 12:13:56 +0100

    Complete beginner program. But there's no way to recieve messages

--- App.elm
+++ App.elm
@@ -4,4 +4,34 @@ import Html
 
 
 main =
-    Html.text "Vackrare frontend med Elm."
+    Html.beginnerProgram { model = 1, view = view, update = update }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    Int
+
+
+type Msg
+    = SetSlideNumber Int
+
+
+
+-- UPDATE
+
+
+update msg model =
+    case msg of
+        SetSlideNumber number ->
+            number
+
+
+
+-- VIEW
+
+
+view model =
+    Html.text ("Vackrare frontend med Elm. Slide number: " ++ toString model)
