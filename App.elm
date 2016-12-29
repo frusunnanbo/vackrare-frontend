dcd612b2dbb50ead4732aeaa304cc4e37ede4622
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2016-12-29 08:01:20 +0100

    Wow. Switching css class with a tiny button.

--- App.elm
+++ App.elm
@@ -1,11 +1,14 @@
 module Main exposing (..)
 
 import Html
+import Html.Attributes
 import Html.Events
+import Html.CssHelpers
+import Styles
 
 
 main =
-    Html.beginnerProgram { model = 1, view = view, update = update }
+    Html.beginnerProgram { model = { theme = Styles.Light, slideNumber = 1 }, view = view, update = update }
 
 
 
@@ -13,11 +16,13 @@ main =
 
 
 type alias Model =
-    Int
+    { theme : Styles.CssClasses, slideNumber : Int }
 
 
 type Msg
     = SetSlideNumber String
+    | DarkTheme
+    | LightTheme
 
 
 
@@ -29,16 +34,26 @@ update msg model =
         SetSlideNumber number ->
             String.toInt number
                 |> Result.withDefault 0
+                |> \slideNumber -> { model | slideNumber = 0 }
+
+        DarkTheme ->
+            { model | theme = Styles.Dark }
+
+        LightTheme ->
+            { model | theme = Styles.Light }
 
 
 
 -- VIEW
 
 
+{ id, class, classList } =
+    Html.CssHelpers.withNamespace "main"
 view model =
-    Html.div []
-        [ Html.text ("Vackrare frontend med Elm. Slide number: " ++ toString model)
+    Html.div [ class [ model.theme ] ]
+        [ Html.h1 [] [ Html.text ("Vackrare frontend med Elm. Slide number: " ++ toString model.slideNumber) ]
         , Html.input
             [ Html.Events.onInput SetSlideNumber ]
             []
+        , Html.button [ Html.Events.onClick DarkTheme ] []
         ]
--- Styles.elm
+++ Styles.elm
@@ -1,8 +1,18 @@
-module Styles exposing (css)
+module Styles exposing (..)
 
 import Css exposing (..)
 import Css.Elements exposing (body)
+import Css.Namespace exposing (namespace)
+
+
+type CssClasses
+    = Dark
+    | Light
 
 
 css =
-    stylesheet [ body [ backgroundColor (hex "CCFFFF") ] ]
+    (stylesheet << namespace "main")
+        [ (.) Dark [ backgroundColor (hex "333333") ]
+        , (.) Light [ backgroundColor (hex "EEEEEE") ]
+        , (#) "main" [ padding (px 10) ]
+        ]
