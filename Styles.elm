513f867271f3a1fb4df86f60587ff2edc0e7a0bf
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2016-12-29 15:35:50 +0100

    Some styling

--- Styles.elm
+++ Styles.elm
@@ -5,14 +5,8 @@ import Css.Elements exposing (body)
 import Css.Namespace exposing (namespace)
 
 
-type CssClasses
-    = Dark
-    | Light
-
-
 css =
-    (stylesheet << namespace "main")
-        [ (.) Dark [ backgroundColor (hex "333333") ]
-        , (.) Light [ backgroundColor (hex "EEEEEE") ]
-        , (#) "main" [ padding (px 10) ]
+    stylesheet
+        [ body [ margin (px 0) ]
+        , (#) "main" [ displayFlex, flexFlow1 column, height (pct 100), width (pct 100) ]
         ]
