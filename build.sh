43aefb5c4a04eef462894daf9cf5cb2b16d0691d
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2017-02-01 22:24:11 +0100

    Moved all .elm file to their own directory

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-elm-css Stylesheets.elm &&
-elm-make App.elm --output app.js &&
-elm-make Hello.elm --output hello.js &&
-elm-make RandomGif.elm --output randomgif.js &&
-elm-make Counter.elm --output counter.js &&
-elm-make SimpleTimer.elm --output simpletimer.js &&
-elm-make PromoCode.elm --output promocode.js
+elm-css src/Stylesheets.elm &&
+elm-make src/App.elm --output app.js &&
+elm-make src/Hello.elm --output hello.js &&
+elm-make src/RandomGif.elm --output randomgif.js &&
+elm-make src/Counter.elm --output counter.js &&
+elm-make src/SimpleTimer.elm --output simpletimer.js &&
+elm-make src/PromoCode.elm --output promocode.js
